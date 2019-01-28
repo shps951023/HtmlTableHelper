541d10e52719709e9901dc5a3b674ae7fd6cb4cb
ITWeiHan <shps951002@gmail.com>
2019-01-28 08:46:38 +0800

    add asp.net core mvc demo and user's guide

--- Samples/AspNetCoreMVCDemo/Models/IHelperExtension.cs
+++ Samples/AspNetCoreMVCDemo/Models/IHelperExtension.cs
@@ -6,10 +6,19 @@ using Microsoft.AspNetCore.Html;
 
 public static class IHelperExtension
 {
-    public static HtmlString ToHtmlTable<T>(this IHtmlHelper htmlHelper
-        , IEnumerable<T> enums)
+    public static HtmlString ToHtmlTable<T>(this IHtmlHelper htmlHelper, IEnumerable<T> enums
+        , object tableAttributes = null, object trAttributes = null, object tdAttributes = null
+        , HtmlTableSetting HTMLTableSetting = null)
     {
-        var html = enums.ToHtmlTable();
+        var html = enums.ToHtmlTable(tableAttributes, trAttributes, tdAttributes, HTMLTableSetting);
+        return new HtmlString(html);
+    }
+
+    public static HtmlString ToHtmlTable<T>(this IHtmlHelper htmlHelper, System.Data.DataTable datatable
+        , object tableAttributes = null, object trAttributes = null, object tdAttributes = null
+        , HtmlTableSetting HTMLTableSetting = null)
+    {
+        var html = datatable.ToHtmlTable(tableAttributes, trAttributes, tdAttributes, HTMLTableSetting);
         return new HtmlString(html);
     }
 }
