a424abef8b3e7f9570215dcd0c6e186e39c5c19d
ITWeiHan <shps951002@gmail.com>
2019-01-29 08:43:59 +0800

    NestedTypeHelper add Null check and add NestedTypeHelperTests

--- Src/ITWeiHan.HtmlTableHelper.Test/CustomAttributeTests.cs
+++ Src/ITWeiHan.HtmlTableHelper.Test/CustomAttributeTests.cs
@@ -1,33 +1,55 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
 
 namespace HtmlTableHelper.Test
 {
-    #region TestData
-    public class Test
-    {
-        [TableColumn(DisplayName = "Column1", Skip =true)]
-        public string MyProperty1 { get; set; }
-    }
-    #endregion
-
     [TestClass]
     public class CustomAttributeTests
     {
-        [TestMethod]
-        public void DisplayName_Test()
+        #region TestData
+        private class Test
         {
-            var excepted = new TableColumnAttribute() { DisplayName = "Column1", Skip = true };
+            [TableColumn(DisplayName = "Column1", Skip = true)]
+            public string MyProperty1 { get; set; }
+            [TableColumn(DisplayName = "Column2", Skip = false)]
+            public string MyProperty2 { get; set; }
+        }
+        #endregion
 
+        [TestMethod]
+        public void GetCustomAttribute_Test()
+        {
+            var excepted = new[]{
+                new TableColumnAttribute() { DisplayName = "Column1", Skip = true },
+                new TableColumnAttribute() { DisplayName = "Column2", Skip = false }
+            };
+            var paras =  new[] { typeof(Test) };
+            var customAttributes = NestedTypeHelper.CallStaticNestedTypeMethod(typeof(HtmlTableHelper)
+                , "CustomAttributeHelper", "GetCustomAttributes", paras) as IEnumerable<TableColumnAttribute>;
+
+            customAttributes.Select((attribute, index) =>
+            {
+                Assert.AreEqual(attribute.DisplayName, excepted[index].DisplayName);
+                Assert.AreEqual(attribute.Skip, excepted[index].Skip);
+                return true;
+            }).ToArray();
+        }
+        
+        [TestMethod]
+        public void RenderHtmlTable_Test()
+        {
             var type = typeof(Test);
-            var datas = NestedTypeHelper.CallStaticNestedTypeMethod(typeof(HtmlTableHelper)
-                , "CustomAttribute", "GetCustomAttributes", new[] { type }) as IEnumerable<TableColumnAttribute>;
-            var data = datas.First();
-
-            Assert.AreEqual(data.DisplayName,excepted.DisplayName);
+            var customAttributes = (NestedTypeHelper.CallStaticNestedTypeMethod(typeof(HtmlTableHelper)
+                , "CustomAttributeHelper", "GetCustomAttributes", new[] { type }) as IEnumerable<TableColumnAttribute>).ToList();
+            var exceptedHtml = @"<table><tr><th>Column1<th><th>Column2<th></tr><tr><td>Row1Column1</td><td>Row1Column2</td></tr><tr><td>Row2Column1</td><td>Row2Column2</td></tr></table>";
+            var sourceData = new Test[] { new Test { MyProperty1 = "Row1Column1", MyProperty2 = "Row1Column2" },
+                new Test { MyProperty1 = "Row2Column1", MyProperty2 = "Row2Column2" }
+            };
+            var th = string.Join(string.Empty, customAttributes.Select(s => $"<th>{s.DisplayName}<th>"));
+            var td = string.Join(string.Empty, sourceData.Select(s => $"<tr><td>{s.MyProperty1}</td><td>{s.MyProperty2}</td></tr>"));
+            var html = $@"<table><tr>{th}</tr>{td}</table>".ToString();
+            Assert.AreEqual(exceptedHtml, html);
         }
     }
 }
