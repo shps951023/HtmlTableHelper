4c0ca338a1ff5e89b0e22c09dbff68bc909e4372
ITWeiHan <shps951002@gmail.com>
2019-01-30 09:32:21 +0800

    Annotation Skip Attribute

--- Src/ITWeiHan.HtmlTableHelper.Test/CustomAttributeTests.cs
+++ Src/ITWeiHan.HtmlTableHelper.Test/CustomAttributeTests.cs
@@ -8,11 +8,18 @@ namespace HtmlTableHelper.Test
     public class CustomAttributeTests
     {
         #region TestData
-        private class Test
+        public class Test
         {
             [TableColumn(DisplayName = "Column1", Skip = true)]
             public string MyProperty1 { get; set; }
             [TableColumn(DisplayName = "Column2", Skip = false)]
+            public string MyProperty2 { get; set; }
+        }
+        public class TestWithoutSkip
+        {
+            [TableColumn(DisplayName = "Column1")]
+            public string MyProperty1 { get; set; }
+            [TableColumn(DisplayName = "Column2")]
             public string MyProperty2 { get; set; }
         }
         #endregion
@@ -31,15 +38,38 @@ namespace HtmlTableHelper.Test
         }
 
         [TestMethod]
+        public void Skip_Attribute_Test()
+        {
+            var excepted = "<table><thead><tr><th>Column2</th></tr></thead><tbody><tr><td>MyProperty1Value2</td></tr><tr><td>MyProperty2Value2</td></tr></tbody></table>";
+            var sourceData = new Test[]{
+                new Test { MyProperty1 = "MyProperty1Value1", MyProperty2 = "MyProperty1Value2" },
+                new Test { MyProperty1 = "MyProperty2Value1", MyProperty2 = "MyProperty2Value2" },
+            };
+            var html = sourceData.ToHtmlTable();
+            Assert.AreEqual(excepted, html);
+        }
+
+        [TestMethod]
+        public void AllPorpsAreWithOutSkipTrue_Test()
+        {
+            var excepted = "<table><thead><tr><th>Column1</th><th>Column2</th></tr></thead><tbody><tr><td>MyProperty1Value1</td><td>MyProperty1Value2</td></tr><tr><td>MyProperty2Value1</td><td>MyProperty2Value2</td></tr></tbody></table>";
+            var sourceData = new TestWithoutSkip[]{
+                new TestWithoutSkip { MyProperty1 = "MyProperty1Value1", MyProperty2 = "MyProperty1Value2" },
+                new TestWithoutSkip { MyProperty1 = "MyProperty2Value1", MyProperty2 = "MyProperty2Value2" },
+            };
+            var html = sourceData.ToHtmlTable();
+            Assert.AreEqual(excepted, html);
+        }
+
+        [TestMethod]
         public void GetCustomAttribute_Test()
         {
             var excepted = new[]{
                 new TableColumnAttribute() { DisplayName = "Column1", Skip = true },
                 new TableColumnAttribute() { DisplayName = "Column2", Skip = false }
             };
-            var paras =  new[] { typeof(Test) };
-            var customAttributes = NestedTypeHelper.CallStaticNestedTypeMethod(typeof(HtmlTableHelper)
-                , "CustomAttributeHelper", "GetCustomAttributes", paras) as IEnumerable<TableColumnAttribute>;
+
+            var customAttributes = HtmlTableHelper.CustomAttributeHelper.GetCustomAttributes(type: typeof(Test));
 
             customAttributes.Select((attribute, index) =>
             {
@@ -48,17 +78,20 @@ namespace HtmlTableHelper.Test
                 return true;
             }).ToArray();
         }
-        
+
+
+
         [TestMethod]
         public void GetCustomAttribute_RenderHtmlTable_Test()
         {
-            var paras = new[] { typeof(Test) };
-            var customAttributes = NestedTypeHelper.CallStaticNestedTypeMethod(typeof(HtmlTableHelper)
-                , "CustomAttributeHelper", "GetCustomAttributes", paras) as IEnumerable<TableColumnAttribute>;
+            
             var exceptedHtml = @"<table><tr><th>Column1<th><th>Column2<th></tr><tr><td>Row1Column1</td><td>Row1Column2</td></tr><tr><td>Row2Column1</td><td>Row2Column2</td></tr></table>";
             var sourceData = new Test[] { new Test { MyProperty1 = "Row1Column1", MyProperty2 = "Row1Column2" },
                 new Test { MyProperty1 = "Row2Column1", MyProperty2 = "Row2Column2" }
             };
+
+            var customAttributes = HtmlTableHelper.CustomAttributeHelper.GetCustomAttributes(type: typeof(Test));
+
             var th = string.Join(string.Empty, customAttributes.Select(s => $"<th>{s.DisplayName}<th>"));
             var td = string.Join(string.Empty, sourceData.Select(s => $"<tr><td>{s.MyProperty1}</td><td>{s.MyProperty2}</td></tr>"));
             var html = $@"<table><tr>{th}</tr>{td}</table>".ToString();
